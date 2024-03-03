5f6c7380cfd4c10c4d641826bac705d8bee35d4c
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2024-03-03 17:22:54 +0100

    add some missing plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -7,6 +7,18 @@ return {
     lazy = false,
     priority = 1000,
   },
+  {
+    "eandrju/cellular-automaton.nvim",
+    cmd = "CellularAutomaton"
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup()
+    end
+  },
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
